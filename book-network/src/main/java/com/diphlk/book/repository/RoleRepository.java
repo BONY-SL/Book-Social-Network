f8a21c2202163fea4b25c2046035632e4284f22e
Dilshan Pathirage <danidutharuka12345@gmail.com>
2026-03-21 23:28:37 +0530

    spring security config

---
+++ book-network/src/main/java/com/diphlk/book/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.diphlk.book.repository;
+
+import com.diphlk.book.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+}
